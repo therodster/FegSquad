03d5b0226018b00100f2ea5a3f1a26bd801c4dd0
Rodrigo <loloe648@gmail.com>
2017-04-20 17:46:47 -0400

    First Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>Login</title>
+    <link type="text/css" href="style.css" rel="stylesheet">
+</head>
+<body>
+    <div class = "page">
+        <div class = "loginbox">
+            <div class = "titlebox">
+                <p class = "title">ARE YOU A MEMBER OF THE FEG SQUAD?</p>
+                <p class = "subtitle">IF SO ENTER YOUR BERGEN USERNAME AND RODDY'S PASSWORD.</p>
+            </div>
+            <form class = "login">
+                <div class = "usernamebox"><p class = "text">USERNAME</p><input type="text" name="username"></div>
+                <div class = "passwordbox"><p class = "text">PASSWORD</p><input type="text" name="password"></div>
+                <div class = "buttonbox"><input class="loginbutton"type="button" name="login" value="LOGIN"></div>
+            </form>
+        </div>
+    </div>
+    <a href="home.html">Some irrelevant link</a>
+</body>
+</html>
